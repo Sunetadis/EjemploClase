4bffc768559bd72bee5ed1fc5c0ce773c03c7fa2
Sunetadis <sunetadis@gmail.com>
2025-06-20 00:26:24 -0600

    Update hola.c
    Se le solicita modificar el printf

--- hola.c
+++ hola.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hola mundo de MAria perez \n");
+    printf("Hola mundo de MAria perez \n"); //Debe corregir este prinfg tiene fallas
     return 0;
 }
 
